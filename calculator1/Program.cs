91efc81bacb68edef73b9eb898d18358b8be0b4a
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 13:25:27 +0100

    simple addition calc

---
+++ calculator1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace calculator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Enter a number:  ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number:  ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double sum = num1+num2;
+            Console.WriteLine("The sum of " + num1 + " and " + num2 +" is "+ sum);
+
+        }
+    }
+}
