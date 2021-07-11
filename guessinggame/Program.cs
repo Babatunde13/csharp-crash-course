50f1b7752edb599b8751c2c480130992c8f78de5
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 15:57:45 +0100

    guessing game

---
+++ guessinggame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace guessinggame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Guess("heyy!");
+        }
+
+        static void Guess(string secretWord) {
+            Console.Write("Enter a guess:  ");
+            string guess = Console.ReadLine();
+            int guessCount = 1;
+            int guessLimit = 5;
+            while (guess != secretWord && guessCount < guessLimit) {
+                int remainingTrials = guessLimit - guessCount;
+                Console.WriteLine("You have "+ remainingTrials+ " left");
+                Console.Write("Enter a guess:  ");
+                guess = Console.ReadLine();
+                guessCount++;
+            }
+            if (secretWord == guess) Console.WriteLine("You Win");
+            else Console.WriteLine("Oops1, You Loose");
+        }
+    }
+}
