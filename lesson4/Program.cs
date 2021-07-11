8b848cf95f1f66ce04185d785fab3a67c95f7f4b
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 13:15:52 +0100

    getting user inputs

---
+++ lesson4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(-40, 4));
+            Console.WriteLine(Math.Max(-40, 4));
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello  "+ name + ", you are " + age + " years old.");
+        }
+    }
+}
