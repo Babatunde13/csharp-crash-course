71412b2e325c1f352f37d067467e615b92c65fe6
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 16:17:01 +0100

    for loop

--- loops/Program.cs
+++ loops/Program.cs
@@ -8,9 +8,12 @@ namespace loops
         {
             Console.WriteLine("Hello World!");
             int num = 0;
+            Console.WriteLine("While Loop");
             WhileLoop(num);
-            Console.WriteLine("");
+            Console.WriteLine("Do While Loop");
             DoWhileLoop(num);
+            Console.WriteLine("For Loop");
+            ForLoop(10);
         }
 
         static void WhileLoop (int num) {
@@ -26,6 +29,13 @@ namespace loops
                 Console.WriteLine(num);
                 num++;
             } while(num < 5);
+        }
+
+        static void ForLoop (int num) {
+            int[] luckyNumbers = {4, 5, 6, 2, 10, 11};
+            for (int i = 0; i < luckyNumbers.Length; i++) {
+                Console.WriteLine(luckyNumbers[i]);
+            }
         }
     }
 }
