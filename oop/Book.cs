3bc2a5c9687bb679f705f1f5a609e9c311eb305c
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 17:08:18 +0100

    intro to classes

---
+++ oop/Book.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace oop
+{
+    /// <summary>
+    /// A Book Class
+    /// @params title: Title of the book
+    /// @params author: Author of the book
+    /// @params pages: Number of pages of the book
+    /// </summary>
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ oop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "Dive Deep Into C#";
+            book1.author = "Babatunde Koiki";
+            book1.pages = 999;
+
+            Book book2= new Book();
+            book2.title = "Dive Deep Into C++";
+            book2.author = "Babatunde M Koiki";
+            book2.pages = 5999;
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book2.author);
+        }
+    }
+}
