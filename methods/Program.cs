e2319cdaebdd97de4457e15321a82fec8a2f9ee0
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 14:06:55 +0100

    methods

---
+++ methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Babatunde", 13);
+            Console.WriteLine(cube(3));
+        }
+
+        // new method
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello "+ name);
+            Console.WriteLine("You are "+ age);
+        }
+
+        static int cube(int num)
+        {
+            return num * num * num;
+        }
+    }
+}
