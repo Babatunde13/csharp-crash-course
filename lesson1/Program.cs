9da782b966f002b68dd3d677e764219bf4fb3d6c
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 12:04:55 +0100

    initial commit: setup

---
+++ lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 25;
+            Console.WriteLine("There was once a man named "+ characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+        }
+    }
+}
