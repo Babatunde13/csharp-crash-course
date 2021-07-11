b37afe4f814881689c5a34436a34eb3fac0ee4ef
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 17:22:32 +0100

    constructors

--- oop/Book.cs
+++ oop/Book.cs
@@ -1,17 +1,23 @@
 using System;
 
-namespace oop
-{
+namespace oop {
     /// <summary>
     /// A Book Class
     /// @params title: Title of the book
     /// @params author: Author of the book
-    /// @params pages: Number of pages of the book
+    /// @params pages: Number of pages of the book.
+    /// <code>Book book1 = new Book("Title", "author author", 1000)</code>
     /// </summary>
-    class Book
-    {
+    class Book {
         public string title;
         public string author;
         public int pages;
+
+        public Book(string title_, string author_, int pages_) {
+            Console.WriteLine("Creating Book");
+            title = title_;
+            author = author_;
+            pages = pages_;
+        }
     }
 }
