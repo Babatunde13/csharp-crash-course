454cd7b41edc6b8dbd207ac5b166ab4d847fef6b
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 15:38:17 +0100

    while and do while loops

---
+++ loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int num = 0;
+            WhileLoop(num);
+            Console.WriteLine("");
+            DoWhileLoop(num);
+        }
+
+        static void WhileLoop (int num) {
+            while(num < 5) {
+                Console.WriteLine(num);
+                num++;
+            }
+        }
+
+        static void DoWhileLoop (int num) {
+            num = 6;
+            do {
+                Console.WriteLine(num);
+                num++;
+            } while(num < 5);
+        }
+    }
+}
