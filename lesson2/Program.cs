983e4d6083fa286944ed139e77478b041f3146eb
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-07-11 12:40:25 +0100

    data types

---
+++ lesson2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            // float, double, decimal_point
+            double gpa = 3.3;
+            bool isMale = true;
+            Console.WriteLine(phrase);
+            Console.WriteLine(grade);
+        }
+    }
+}
